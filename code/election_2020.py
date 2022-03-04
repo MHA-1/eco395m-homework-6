0a9c9219e1eb1ce420e6194d76122beefbd3b06b
mohammed <mhmq.99@gmail.com>
2022-03-04 00:01:56 -0600

    Solve election results.

--- code/election_2020.py
+++ code/election_2020.py
@@ -5,32 +5,57 @@ IN_PATH = os.path.join("data", "countypres_2000-2020.csv")
 OUTPUT_DIR = "artifacts"
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "election_report.csv")
 
-
 def count_votes(path):
 
-    # you write this
+    with open(path, "r") as in_file:
+        dict_reader = csv.DictReader(in_file)
+
+        counts = {}
+
+        for row in dict_reader:
+            year = row['year']
+            state = row['state_po']
+            candidate = row['candidate']
+        
+            if year == '2020':
+                try:
+                    candidate_votes = int(row['candidatevotes'])
+                    if (year, state, candidate) in counts:
+                        counts[year, state, candidate] += candidate_votes
+                    else:
+                        counts[year, state, candidate] = candidate_votes
+                except ValueError:
+                    pass
 
     return counts
 
-
 def get_rows(counts):
-
-    # you write this
+    
+    rows = list()
+    for k in counts:
+        tt = list(k)
+        ff = tt.append(counts[k])
+        rows.append(tt)
 
     return rows
 
+def sort_rows(list_):
+    
+    list_.sort(key=lambda x:x[3], reverse = True)
+    list_.sort(key=lambda x:x[1])
 
-def sort_rows(rows):
-
-    # you write this
-
-    return rows_lex_ordered
-
+    return list_
 
 def write_rows(rows):
 
-    # you write this
+    with open(OUTPUT_PATH, 'w+') as out_file:
+        csv_writer = csv.writer(out_file)
 
+        header = ['Year', 'State_code', 'Candidate', 'Votes']
+        data = rows 
+
+        csv_writer.writerow(header)
+        csv_writer.writerows(data)
 
 if __name__ == "__main__":
 
--- code/pandas_election_2020.py
+++ code/pandas_election_2020.py
@@ -5,5 +5,17 @@ IN_PATH = os.path.join("data", "countypres_2000-2020.csv")
 OUTPUT_DIR = "artifacts"
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "election_report_pandas.csv")
 
+df = (
+    pd.read_csv(IN_PATH)
+    .loc[lambda df: df['year'] == 2020]
+    .loc[lambda df: ~(df['totalvotes'].isna())]
+    )
+
+filtered_ = df.loc[:, ['year', 'state_po', 'candidate', 'candidatevotes']]
+
+df_votesum = filtered_.groupby(['year', 'state_po', 'candidate'])['candidatevotes'].sum().to_frame()
+
+sorted_votes = df_votesum.sort_values(['state_po', 'candidatevotes'], ascending = (True, False))
+
 if __name__ == "__main__":
-    # you write this
+    sorted_votes.to_csv(OUTPUT_PATH)
